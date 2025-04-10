d56875a986c15bb0d0986c530b3cca8fc7ead915
sirloreal <gary.page3010@gmail.com>
2025-04-10 14:47:40 +0100

    All gutterballs

---
+++ BowlingGame/BowlingGame.cs
@@ -0,0 +1,21 @@
+﻿namespace BowlingGame
+{
+    public class BowlingGame
+    {
+        public BowlingGame()
+        {
+
+        }
+
+        public void RollFrame(int firstRoll, int secondRoll)
+        {
+            // Logic to roll a frame with the given pins
+        }
+
+        public int Score()
+        {
+            // Logic to calculate the score
+            return 0;
+        }
+    }
+}
--- BowlingGameTests/BowlingGameTests.cs
+++ BowlingGameTests/BowlingGameTests.cs
@@ -3,9 +3,14 @@ namespace BowlingGameTests
     public class BowlingGameTests
     {
         [Fact]
-        public void Hookup()
+        public void GutterBalls() // Whole game of gutterballs
         {
-            Assert.Fail();
+            var game = new BowlingGame.BowlingGame();
+            for (int i = 0; i < 10; i++)
+            {
+                game.RollFrame(0,0);
+            }
+            Assert.Equal(0, game.Score());
         }
     }
 }
