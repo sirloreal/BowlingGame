f496b55caa8c7d146cde9aec0c50a5747c2b4a64
sirloreal <gary.page3010@gmail.com>
2025-04-10 16:14:57 +0100

    Refactor a little more

--- BowlingGame/BonusFrame.cs
+++ BowlingGame/BonusFrame.cs
@@ -13,6 +13,8 @@ namespace BowlingGame
 
         public override int Score()
         {
+            // With this implementation the bonus frame always follows a strike or a spare. 
+            // Because the Strike and Spare classes are responsible for adding the extra throws, we don't need to do that here.
             return 0;
         }
     }
