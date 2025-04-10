886424cb028b30a7dcd15af100e9fd7415d2a796
sirloreal <gary.page3010@gmail.com>
2025-04-10 15:21:07 +0100

    Dynamically calculate Frame score

--- BowlingGame/Frame.cs
+++ BowlingGame/Frame.cs
@@ -5,7 +5,6 @@ namespace BowlingGame
     internal class Frame : IFrame
     {
         private ArrayList throws;
-        private int score;
         private int startingThrow;
 
         public Frame(ArrayList throws, int startingThrow, int firstRoll, int secondRoll)
@@ -14,12 +13,11 @@ namespace BowlingGame
             this.startingThrow = throws.Count;
             throws.Add(firstRoll);
             throws.Add(secondRoll);
-            this.score = firstRoll + secondRoll;
         }
 
         public int Score()
         {
-            return score;
+            return (int)throws[startingThrow] + (int)throws[startingThrow+1];
         }
     }
 }
